78103c4da491c984d449da7a60d415cb5e7473bb
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2025-03-17 21:04:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,8 +57,8 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://GojoSand:0987@cluster0.jg6pyug.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Gojo")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'gojo')
+DATABASE_NAME = environ.get('DATABASE_NAME', "AutoFilter")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #refer time, or user count
 REFERAL_USER_TIME = int(environ.get('REFERAL_USER_TIME', "2592000")) # set in seconds | already seted 1 month premium
